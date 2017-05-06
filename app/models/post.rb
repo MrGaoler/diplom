2f0c0916003d37f89fee37872fca16e2f7bc3b18
MrGaoler <notwronglife@gmail.com>
2017-05-06 21:23:29 +0300

    #slimise_add_validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates_uniqueness_of :email, :username
+  validates_presence_of :email, :fname, :lname, :password, :username
+  validates_length_of :password, in: 8..20
+  validates_confirmation_of :password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+100.times do |i|
+  user = User.create(
+    fname: "first#{i}",
+    lname: "last#{i}",
+    username: "username#{i}",
+    email: "test#{i}@example.com",
+    password: 'password',
+  )
+  user.posts.create(
+    title: "Hello from #{user.id}",
+    body: SecureRandom.hex,
+    created_at: Time.now
+  )
+end
