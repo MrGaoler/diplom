506ff9aa4637a4db8327d74c35fae34a27e46abc
Serhii Ovcharenko <sergiy20101995@ukr.net>
2017-05-06 20:26:46 +0300

    #add_admin_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'home#show'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  scope '/' do
+    resource :home, only: :show
+    resource :about, only: :show
+  end
 
   scope '/forum' do
     resources :posts
   end
+
   devise_for :users
 end
