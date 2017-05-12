f99ba8e7ca861d2dcb996d263bf2f486ac727bf2
Serhii Ovcharenko <sergiy20101995@ukr.net>
2017-05-13 02:10:20 +0300

    add locals example

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Diplom
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Whitelist locales available for the application
+    I18n.available_locales = %i[en ua]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :ua
   end
 end
