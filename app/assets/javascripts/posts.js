7711c8c2d811490f8be5709dfbea65dd4505dbaa
MrGaoler <notwronglife@gmail.com>
2017-05-15 00:03:56 +0300

    blog_theme

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("#switch-view").click(function(){
+    $(this).find("button").toggleClass("active");
+    $(".article-wrapper").toggleClass("bloc col-xs-12 col-xs-4");
+  });
+});
