15d453850517d49ac355048d34fcee5bd7d4e94f
Serhii Ovcharenko <sergiy20101995@ukr.net>
2017-05-26 23:27:59 +0300

    fix opening image in new window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,14 @@
 //= require bootstrap-sprockets
 //= require jquery.remotipart
 //= require jquery_ujs
+//= require lightbox-bootstrap
 //= require_tree .
 
 $(document).on('ready', function() {
-    setTimeout("$('.alert').fadeOut('slow')", 3000);
+  setTimeout("$('.alert').fadeOut('slow')", 3000);
+
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
 });
