824065261df356dc1202c6013157220eb2209c31
MrGaoler <notwronglife@gmail.com>
2017-05-28 23:54:02 +0300

    add seeds

--- app/models/service.rb
+++ app/models/service.rb
@@ -22,5 +22,5 @@ class Service < ApplicationRecord
   has_one :image, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :image
 
-  validates_presence_of :name, :level, :duration, :description
+  validates_presence_of :name, :level, :description
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 User.destroy_all
 Post.destroy_all
 Member.destroy_all
+Image.destroy_all
 Cms::StaticPage.destroy_all
 
 10.times do |i|
@@ -34,12 +35,41 @@ User::ROLES.each do |role_name|
 end
 
 10.times do
-  Member.create!(
+  Member.create(
     full_name: Faker::Name.name,
     position: Faker::Job.field
-  )
+  ).build_image.save
+end
+10.times do |i|
+  services = Service.create!(
+    name: Faker::Name.name,
+    level: Faker::Demographic.race,
+    description: Faker::Demographic.race
+  ).build_image.save
 end
 
 about_page = Cms::AboutPage.load
 about_page.description = Faker::Job.title
 about_page.save
+
+
+10.times do |i|
+  services = Service.create!(
+    name: Faker::Name.name,
+    level: Faker::Demographic.race,
+    description: Faker::Demographic.race
+  ).build_image.save
+end
+
+services_page = Cms::ServicesPage.load
+services_page.title = Faker::Job.title
+services_page.description = Faker::Job.title
+services_page.save
+
+contacts_page = Cms::ContactsPage.load
+contacts_page.phone = Faker::PhoneNumber.cell_phone
+contacts_page.email = Faker::Internet.email
+contacts_page.location = Faker::Address.street_address
+contacts_page.work_time = Faker::Time.between(DateTime.now - 1, DateTime.now)
+contacts_page.save
+
