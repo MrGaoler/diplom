2b7d31c83551c34e50ca64989c809fe1bfb58b8a
Serhii Ovcharenko <sergiy20101995@ukr.net>
2017-06-04 02:18:10 +0300

    fix posts images

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,25 +1,23 @@
-$(window).scroll(function (){
+if($('.home')) {
 
-  //Header Paralax
-  var wScroll = $(this).scrollTop();
+  $(window).scroll(function () {
 
-  $('header h1').css({
-    'transform' : 'translate(0px, '+ wScroll / 3 +'px)'
-  });
+    //Header Paralax
+    var wScroll = $(this).scrollTop();
 
-  //Landing Elements
-  if(wScroll > $('.clothes').offset().top - ($(window).height() / 1.2)) {
-
-    $('.clothes figure').each(function(i){
-
-      //Delay
-      setTimeout(function(){
-        $('.clothes figure').eq(i).addClass('show');
-      }, 150 * (i+1));
-
+    $('header h1').css({
+      'transform': 'translate(0px, ' + wScroll / 3 + 'px)'
     });
 
-  }
+    //Landing Elements
+    if (wScroll > $('.clothes').offset().top - ($(window).height() / 1.2)) {
+
+      $('.clothes figure').each(function (i) {
+
+      });
+
+    }
 
 
-});
+  });
+}
